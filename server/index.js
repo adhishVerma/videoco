58d9ad113117d9709bf0fd4e3451e4b5752b617a
adhishVerma <v.adhish16@gmail.com>
2023-03-24 23:08:55 +0530

    express port changed

--- server/index.js
+++ server/index.js
@@ -67,6 +67,6 @@ io.on('connection', socket => {
 })
 
 
-const PORT = 5000 || process.env.PORT
+const PORT =  process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`))
