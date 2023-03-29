2726b6eca22304f342e3b257be5ecbe734efc592
adhishVerma <v.adhish16@gmail.com>
2023-03-29 16:13:40 +0530

    add: .catch

--- server/controllers/getIce.js
+++ server/controllers/getIce.js
@@ -4,11 +4,8 @@ const getIce = (req,res) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
-    try{
-        client.tokens.create().then(token => res.status(200).json(token.iceServers));
-    }catch(err){
-        console.log(err)
-    }
+
+    client.tokens.create().then(token => res.status(200).json(token.iceServers)).catch(err => console.log(err));
 };
 
 module.exports = {getIce}
