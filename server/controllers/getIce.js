b795dc4cfd90f0910e2424a3d35b65add9aaa2fe
adhishVerma <v.adhish16@gmail.com>
2023-03-29 16:07:57 +0530

    add: try-catch block for IceServers

--- server/controllers/getIce.js
+++ server/controllers/getIce.js
@@ -4,8 +4,11 @@ const getIce = (req,res) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
-
-    client.tokens.create().then(token => res.status(200).json(token.iceServers));
+    try{
+        client.tokens.create().then(token => res.status(200).json(token.iceServers));
+    }catch(err){
+        console.log(err)
+    }
 };
 
 module.exports = {getIce}
