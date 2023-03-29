aff02de132a352b736a83c393f7c8a98e58c31ec
adhishVerma <v.adhish16@gmail.com>
2023-03-29 16:20:39 +0530

    ES6 twilio import

--- server/controllers/getIce.js
+++ server/controllers/getIce.js
@@ -1,9 +1,9 @@
-require("dotenv").config()
+import twilio from 'twilio'
 
 const getIce = (req,res) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
-    const client = require('twilio')(accountSid, authToken);
+    const client = twilio(accountSid, authToken);
 
     client.tokens.create().then(token => res.status(200).json(token.iceServers)).catch(err => console.log(err));
 };
